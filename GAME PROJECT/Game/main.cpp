c3ac9c7e3800fdd5ced6bc44c05cd077222a672b
Fazle_Rakib <fazle30@student.sust.edu>
2018-11-01 21:14:21 +0600

    Update-1

--- GAME PROJECT/Game/main.cpp
+++ GAME PROJECT/Game/main.cpp
@@ -5,20 +5,32 @@
 using namespace std;
 int main()
 {
+    enum Direction{Down,Left,Right,Up};
+
+    sf::Vector2i source(1,Down); // WE can handle source by source.x & source.y
+
     sf::RenderWindow window;
-    window.create(sf::VideoMode(800,600),"Bomber Friends");
+    window.create(sf::VideoMode(925,600),"Bomber Friends");
 
     //window.setKeyRepeatEnabled(false);
 
     sf::Texture textureFrame;
     sf::Texture texture;
-    textureFrame.loadFromFile("data/image/frame.png");
-    texture.loadFromFile("data/image/char.png");
+    sf::Texture textureGrass;
+
+
+    textureFrame.loadFromFile("data/image/frame1.png");
+    texture.loadFromFile("data/image/kavi.png");
+    textureGrass.loadFromFile("data/image/grass.png");
+
 
     sf::Sprite frame(textureFrame);
     frame.setPosition(0,0);
-    sf::Sprite playerImage(texture,sf::IntRect(0,0,50,70));
-    playerImage.setPosition(0,0);
+    sf::Sprite playerImage(texture); //,sf::IntRect(0,0,35,50)
+    playerImage.setPosition(186,50);
+    sf::Sprite grass(textureGrass);
+    grass.setPosition(125,0);
+
 
     /*sf::Font font;
     if(!font.loadFromFile("data/font/ALGER.TTF"))
@@ -38,25 +50,36 @@ int main()
         {
             switch(event.type)
             {
-               case sf::Event::Closed:
+            case sf::Event::Closed:
                 window.close();
                 break;
-            }
-            switch(event.type)
-            {
             case sf::Event::KeyPressed:
-                if(event.key.code == sf::Keyboard::Left)
-                    playerImage.move(-5,0);
-                if(event.key.code == sf::Keyboard::Right)
-                    playerImage.move(5,0);
-                if(event.key.code == sf::Keyboard::Up)
-                    playerImage.move(0,-5);
-                if(event.key.code == sf::Keyboard::Down)
+                if(event.key.code == sf::Keyboard::Down){
+                    source.y = Down;
                     playerImage.move(0,5);
-
-
+                }
+                else if(event.key.code == sf::Keyboard::Left){
+                    source.y = Left;
+                    playerImage.move(-5,0);
+                }
+                else if(event.key.code == sf::Keyboard::Right){
+                    source.y = Right;
+                    playerImage.move(+5,0);
+                }
+                else if(event.key.code == sf::Keyboard::Up){
+                    source.y = Up;
+                    playerImage.move(0,-5);
+                }
+                    break;
             }
+            source.x++;
+            if(source.x*32 >= texture.getSize().x)
+                source.x = 0;
+
             window.draw(frame);
+
+            window.draw(grass);
+            playerImage.setTextureRect(sf::IntRect(source.x * 32,source.y * 48,32,48));
             window.draw(playerImage);
             window.display();
             window.clear();
